39f0dd1502b66d6a5e1daf605b46e938a03cb9bf
robmurray <rmurray5@buffalo.edu>
2025-04-21 13:02:38 -0400

    Split into x and y

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
 import sys
 import numpy as np
-
-
-
-
-
-## Examples taken from assignments
-
-def loadDataSet(fileName):      #general function to parse tab -delimited floats
-    dataMat = []                #assume last column is target value
-    fr = open(fileName)
-    for line in fr.readlines():
-        curLine = line.strip().split(',')
-        dataMat.append(curLine)
-    return np.array(dataMat)
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 
 if __name__ == '__main__':
@@ -23,7 +11,18 @@ if __name__ == '__main__':
         data_filename = 'game_of_thrones_train.csv'
 
     print('Loading data...')
-    dataMat = loadDataSet(data_filename)
+    df = pd.read_csv(data_filename)
+
+    x = df.drop(['isAlive'], axis=1, errors='ignore')
+    y = df['isAlive']
+
+    x = x.to_numpy()
+    y = y.to_numpy()
+
+    print('X:', x.shape)
+    print('X:', y.shape)
+
+
 
 
 
