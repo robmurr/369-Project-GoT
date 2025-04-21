18ae2bde6cf781edeea0d5b79a60f785d04448ca
18Mayas <mlabuhn18@gmail.com>
2025-04-21 13:18:48 -0400

    bayes function

--- main.py
+++ main.py
@@ -2,7 +2,29 @@ import sys
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import GaussianNB
 
+from sklearn.model_selection import train_test_split
+
+
+
+
+def bayes(X, y, test_size, random_state):
+
+    X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size, random_state)
+
+    # Build a Gaussian Classifier
+    model = GaussianNB()
+
+    # Model training
+    model.fit(X_train, y_train)
+
+    # Predict Output
+    predicted = model.predict([X_test[6]])
+
+    print("Actual Value:", y_test[6])
+    print("Predicted Value:", predicted[0])
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
@@ -21,6 +43,8 @@ if __name__ == '__main__':
 
     print('X:', x.shape)
     print('X:', y.shape)
+
+    bayes(x, y, test_size=.33, random_state=125)
 
 
 
