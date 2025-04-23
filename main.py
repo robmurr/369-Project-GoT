5188c55cd42a6280d83f368416e238e4fcc36c7e
robmurray <rmurray5@buffalo.edu>
2025-04-23 15:33:25 -0400

    Updated preprocessing and training method | improved confusion matrix

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
@@ -24,25 +25,29 @@ def bayes(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=125)
 
     # Build a Gaussian Classifier
-    model = GaussianNB()
+    # model = GaussianNB()
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
 
     # Model training
     model.fit(X_train, y_train)
 
     # Predict Output
-    y_pred = model.predict(X_test)
+    # y_pred = model.predict(X_test)
+    y_pred = model.predict(X_train)
 
     # print("Actual Value:", y_test)
     # print("Predicted Value:", y_pred)
 
     # Calculate values
-    accuracy = accuracy_score(y_pred, y_test)
-    conf_matrix = confusion_matrix(y_test, y_pred)
-    class_report = classification_report(y_test, y_pred)
+    # accuracy = accuracy_score(y_pred, y_test)
+    accuracy = accuracy_score(y_train, y_pred)
+    # conf_matrix = confusion_matrix(y_test, y_pred)
+    conf_matrix = confusion_matrix(y_train, y_pred)
+    # class_report = classification_report(y_test, y_pred)
 
     print("Accuracy:", accuracy)
-    print("Confusion Matrix:", conf_matrix)
-    print("Classification Report:", class_report)
+    print("Confusion Matrix:\n", conf_matrix)
+    # print("Classification Report:\n", class_report)
 
     # Create matplot figure
     plt.figure(figsize=(8,6))
@@ -51,8 +56,8 @@ def bayes(X, y):
     plt.title("Confusion Matrix")
     plt.xlabel("Predicted")
     plt.ylabel("Actual")
-    plt.savefig("ConfusionMatrix.png")
-    plt.show()
+    # plt.savefig("ConfusionMatrix.png")
+    # plt.show()
     plt.close()
 
 
@@ -65,7 +70,7 @@ def install_requirements():
         # 'scikit-learn==1.6.1'
         'numpy>=1.28.0',
         'pandas>=2.2.3',
-        'seaborn>=0.14.0',
+        'seaborn==0.14.0',
         'matplotlib>=3.9.2',
         'scikit-learn>=1.5.2'
     ]
@@ -78,12 +83,13 @@ def install_requirements():
             try:
                 subprocess.check_call([sys.executable, '-m', 'pip', 'install', req])
             except subprocess.CalledProcessError as e:
+                print(e.output)
                 sys.exit(1)
 
 
 if __name__ == '__main__':
     # Install requirements
-    install_requirements()
+    # install_requirements()
 
     # Read arguments
     if len(sys.argv) == 2:
@@ -97,6 +103,7 @@ if __name__ == '__main__':
 
 
     # Preprocessing
+    print('Preprocessing data...')
     # Numeric Columns
     numeric_cols = ['age', 'dateOfBirth', 'popularity', 'numDeadRelations']
     for col in numeric_cols:
@@ -108,13 +115,13 @@ if __name__ == '__main__':
     binary_cols = ['isAliveMother', 'isAliveFather', 'isAliveHeir', 'isAliveSpouse', 'male',
                    'isMarried', 'isNoble', 'book1', 'book2', 'book3', 'book4', 'book5']
     for col in binary_cols:
+        # df[col] = df[col].astype(int)
         if col in df.columns:
             df[col] = df[col].fillna(0)
 
 
     # Categorical columns
     # categorical_cols = ['title', 'culture', 'mother', 'father', 'heir', 'house', 'spouse']
-    # df = pd.get_dummies(df, columns=categorical_cols, dummy_na=True)
 
     df.drop(['name', 'S.No'], axis=1, inplace=True, errors='ignore')
     df.drop(['title', 'culture', 'mother', 'father', 'heir', 'house', 'spouse'], axis=1, inplace=True)
@@ -125,9 +132,12 @@ if __name__ == '__main__':
     x = x.to_numpy()
     y = y.to_numpy()
 
-    # scaler = StandardScaler()
-    # x = scaler.fit_transform(x)
+    # Turns everything to scalar
+    scaler = StandardScaler()
+    x = scaler.fit_transform(x)
+    print(df.shape)
 
+    # Calls the main bayes classifier
     bayes(x, y)
 
 
